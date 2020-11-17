161ad0af2a7e3f1364dec324f9450bf6d3ac6c8b
Andrey Costa <andreycode@hotmail.com>
2020-11-16 23:52:12 -0300

    Projeto triangulo finalizado

---
+++ Medias de dois triangulos/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace Medias_de_dois_triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opc = "s";
+
+            while (opc == "s" || opc == "S")
+            {
+
+                Console.WriteLine("Entre com as medidas do triángulo X ");
+
+                Console.Write("Lado 1: ");
+                double xa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Lado 2: ");
+                double xb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Lado 3: ");
+                double xc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Triangulos X = new Triangulos(xa, xb, xc);
+
+                Console.WriteLine();
+                Console.WriteLine("Entre com as medidas do Triángulo Y ");
+                Console.Write("Lado 1: ");
+                double ya = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Lado 2: ");
+                double yb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Lado 3: ");
+                double yc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Triangulos Y = new Triangulos(ya, yb, yc);
+
+                double areaX = X.CalculoTriangulo();
+                double areaY = Y.CalculoTriangulo();
+
+                Console.WriteLine();
+                Console.WriteLine("Área de X = " + X);
+                Console.WriteLine("Área de y = " + Y);
+
+                if (areaX > areaY)
+                {
+                    Console.WriteLine("Triángulo maior é = " + areaX.ToString("f4", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("Triángulo Y é o maior = " + areaY.ToString("f4", CultureInfo.InvariantCulture));
+                }
+                Console.WriteLine();
+                Console.Write("Deseja calcular outros dois triangulos (s/n) ? ");
+                opc = Console.ReadLine();
+
+            }
+
+            
+
+
+            
+        }
+    }
+}
---
+++ Medias de dois triangulos/Triangulos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Medias_de_dois_triangulos
+{
+    class Triangulos
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+       
+        public Triangulos(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double CalculoTriangulo()
+        {
+            double P = (A + B + C) / 2.0;
+            return P = Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+        }
+
+        public override string ToString()
+        {
+            return CalculoTriangulo().ToString("f4", CultureInfo.InvariantCulture);
+        }
+    }
+}
